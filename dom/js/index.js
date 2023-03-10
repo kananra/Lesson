9e3ebc9a5ed7a1d2c09adf8e940e01d4732de2de
Kanan <118839498+kananra@users.noreply.github.com>
2023-03-10 21:40:29 +0400

    new

---
+++ dom/js/index.js
@@ -0,0 +1,12 @@
+
+
+
+document.querySelector(".myBtn").addEventListener("click",myFunction);
+
+
+function myFunction() {
+  document.querySelector(".menu").classList.toggle("dynamic");
+  document.querySelector("img").src=".assets/icons/x-solid.svg"
+}
+
+
